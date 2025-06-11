db1f656d092d1826c0b0a86057c9cd6e1f7cce34
iKripto <yagosiqueira450@gmail.com>
2025-06-10 21:23:01 -0300

    Lanchonete Java

---
+++ Problema09/src/Lanchonete.java
@@ -0,0 +1,24 @@
+public class Lanchonete {
+    private int codigo;
+    private int quantidade;
+
+    public Lanchonete(int codigo, int quantidade) {
+        this.codigo = codigo;
+        this.quantidade = quantidade;
+    }
+
+    public double calcularTotal() {
+        double preco = 0.0;
+
+        switch (codigo) {
+            case 1: preco = 5.00; break;
+            case 2: preco = 3.50; break;
+            case 3: preco = 4.80; break;
+            case 4: preco = 8.90; break;
+            case 5: preco = 7.32; break;
+            default: preco = 0.0; // código inválido pode ser tratado se quiser
+        }
+
+        return preco * quantidade;
+    }
+}
